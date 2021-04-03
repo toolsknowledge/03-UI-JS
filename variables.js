ed169474fd6fcc4d0c9a8ac5bbe5e68560ee8f7d
Sambasivarao <samba.pendela@gmail.com>
2021-04-03 18:48:51 +0530

    code

---
+++ variables.js
@@ -0,0 +1,36 @@
+//string
+//collection of "characters" called as "string"
+//"",   '' and ``
+//`` operator introduced in "ES6"
+//`` operator used to represent the "paragraphs"
+var sub = "Fullstack UI";
+var wish = `Welcome to ${sub}`;
+console.log( wish );  //Welcome to Fullstack UI
+
+
+
+var frondend = "ReactJS";
+var backend = "NodeJS";
+var database = "MongoDB";
+var mernStack = `
+    MERN Stack Means
+    ++++++++++++++++++++++++++++++++++++++++++++
+    ${frondend} <==> ${backend} <==> ${database}
+    ++++++++++++++++++++++++++++++++++++++++++++
+`;
+console.log( mernStack );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
