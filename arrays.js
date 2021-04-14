46bb3b64d87a5c22f2c92aeaf27477564da032f2
Samba <samba.pendela@gmail.com>
2021-04-14 18:55:46 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -91,16 +91,180 @@ console.log(
 */
 
 
-let arr1 = [1,2,3,4,5,6,7,8,9,10];
-arr1.splice(4,2);
-console.log(arr1);    //[1,2,3,4,7,8,9,10]
+/*
+    let arr1 = [1,2,3,4,5,6,7,8,9,10];
+    arr1.splice(4,2);
+    console.log(arr1);    //[1,2,3,4,7,8,9,10]
 
-arr1.splice(4,1);
-console.log(arr1);      //[1,2,3,4,8,9,10]
+    arr1.splice(4,1);
+    console.log(arr1);      //[1,2,3,4,8,9,10]
 
-arr1.splice(6,1);
-arr1.splice(0,1);
-console.log(arr1);      //[ 2, 3, 4, 8, 9 ]
+    arr1.splice(6,1);
+    arr1.splice(0,1);
+    console.log(arr1);      //[ 2, 3, 4, 8, 9 ]
+*/
+
+
+
+/*
+    //findIndex()
+    //it is used to know the "index" of a particular element
+    let arr = [10,20,30,40,50,60,70,80,90,100];
+
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element === 50;
+        })
+    );      //4
+
+
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element === 100;
+        })
+    );      //9
+
+
+    //delete "60"
+    arr.splice(arr.findIndex((element,index)=>{
+        return element === 60;
+    }),1);
+    console.log(arr);
+
+
+    arr.splice(arr.findIndex((element,index)=>{
+        return element === 30;
+    }),1);
+    console.log( arr );
+*/
+
+
+
+/*
+    //delete
+    //"delete" property, used to "delete" the element from "array"
+    //delete property, deletes the "element" and won't release the "memory"
+    let arr = [10,20,30,40,50,60,70,80,90,100];
+    console.log( arr.length );    //10
+
+    delete arr[4];
+    console.log( arr.length );      //10
+
+
+    delete arr[2];
+    console.log( arr.length );      //10
+*/
+
+
+/*
+    //slice
+    //slice function, used to pick the particular element from array.
+    let arr = [10,20,30,40,50,60,70,80,90,100];
+    console.log( arr.slice(4,6) );                      //[ 50, 60 ]
+    console.log( arr.slice(3,6) );                      //[ 40, 50, 60 ]
+    console.log( arr.slice(0,1) );                      //[ 10 ]
+    console.log( arr.slice(9,10) );                     //[ 100 ]
+    console.log(
+        [ ...arr.slice(5,10),...arr.slice(0,5) ].reduce((firstValue,nextValue)=>{
+            return firstValue+nextValue;
+        })
+    );    //550
+*/
+
+
+/*
+    //sort()
+    //ascending order / decending order
+
+    // arg1-arg2   (ascending order)
+    // arg2-arg1   (decending order)
+
+    let arr1 = [10,5,4,2,1,8,7];
+
+
+    //2nd min element
+    console.log(
+        arr1.sort((arg1,arg2)=>{
+            return arg1-arg2;
+        })[1]
+    );     //2
+
+
+    //2md max element
+    console.log(
+        arr1.sort((arg1,arg2)=>{
+            return arg2-arg1;
+        })[1]
+    );    //8
+*/
+
+
+
+/*
+    //some()
+    //if atleast one element satisfies condition, then it will return "true" otherwise "false"
+    console.log(
+        [1,2,3,4,5].some((element,index)=>{
+            return element >= 5;
+        })
+    );          //true
+*/
+
+
+//every()
+//if all elements satisfies the condition then it will return true otherwise false
+console.log(
+    [10,20,30,40,50].every((element,index)=>{
+        return element>=10;
+    })
+);     //true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
